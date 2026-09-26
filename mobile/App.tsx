311508f3719e560e071639deb33759a0ead4e796
ahmad-DS <ahmad.shakil444@gmail.com>
2026-09-26 12:38:00 +0530

    chore: update app display text

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi Arhaan Miyan</Text>
+      <Text>Shakil Ahmad</Text>
       <StatusBar style="auto" />
     </View>
   );
